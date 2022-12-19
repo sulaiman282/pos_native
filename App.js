5b714082e48bb1c05a142c3bca8b39703f0ee01a
sulaiman282 <spidy.twitch.tv@gmail.com>
2022-12-19 13:34:41 +0600

    search filter added

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  Modal,
+  Pressable,
 } from "react-native";
 import NotchDetector from "./Global/notchDetector";
 import {
@@ -18,13 +20,15 @@ import {
 } from "react-native-paper";
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-
+import { AntDesign } from "@expo/vector-icons";
 export default function App() {
   const [text, setText] = useState("");
   const [totalCost, setTotalCost] = useState("");
   const [addedItem, setAddedItem] = useState([]);
   const [testItem, setTestItem] = useState([]);
   const [helperState, setHelperState] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
   var sum = 0;
 
   const priceList = [
@@ -48,7 +52,8 @@ export default function App() {
       price: 60,
       img: "https://dr6svciluuqrd.cloudfront.net/products/GY06qNRoWhWP7e5jyIRonEyzeI2X42sCqmdoD7GB.png",
       count: 1,
-    }, {
+    },
+    {
       id: "125",
       name: "Dark Chocolate",
       price: 150,
@@ -110,10 +115,51 @@ export default function App() {
     setTotalCost("");
   };
 
+  const [filteredDataSource, setFilteredDataSource] = useState(priceList);
+  const [masterDataSource, setMasterDataSource] = useState(priceList);
+  const [search, setSearch] = useState("");
+  const searchFilterFunction = (text) => {
+    if (text) {
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      <View
+        style={{
+          height: 0.5,
+          width: "100%",
+          backgroundColor: "#C8C8C8",
+        }}
+      />
+    );
+  };
+  const ItemView = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => testfun(item.id)}>
+        <View style={{ flexDirection: "row",justifyContent:"space-between" ,marginTop:10}}>
+          <Text style={{ width: "70%",fontSize:20 }}>{item.name}</Text>
+          <Text style={{ width: "20%",fontSize:20 }}>{item.price} TK</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <NotchDetector>
       <StatusBar translucent backgroundColor="blue" style="light" />
       <View>
+        <View>
           <Text
             style={{
               textAlign: "center",
@@ -126,101 +172,167 @@ export default function App() {
           </Text>
         </View>
 
-        <View style={{ flexDirection: "row", width: "100%" }}>
-            <TextInput
-              style={{ width: "100%" }}
-              keyboardType="numeric"
-              label="Enter BAR CODE"
-              labelColor="#0320fc"
-              selectionColor="#0320fc"
-              underlineColor="#0320fc"
-              activeOutlineColor="#0320fc"
-              textColor="#0320fc"
-              activeUnderlineColor="#0320fc"
-              clearButtonMode="always"
-              returnKeyType="done"
-              autoFocus={true}
-              value={text}
-              blurOnSubmit={false}
-              onChangeText={(value) => {
-                setText(value), testfun(value);
-              }}
-              
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            style={{ width: "85%", marginLeft: 5 }}
+            keyboardType="numeric"
+            label="Enter BAR CODE"
+            labelColor="#0320fc"
+            selectionColor="#0320fc"
+            underlineColor="#0320fc"
+            activeOutlineColor="#0320fc"
+            textColor="#0320fc"
+            activeUnderlineColor="#0320fc"
+            clearButtonMode="always"
+            returnKeyType="done"
+            autoFocus={true}
+            value={text}
+            blurOnSubmit={false}
+            onChangeText={(value) => {
+              setText(value), testfun(value);
+            }}
+          />
+          <TouchableOpacity>
+            <AntDesign
+              name="pluscircle"
+              size={24}
+              color="black"
+              style={{ marginRight: 10 }}
+              onPress={() => setModalVisible(true)}
             />
-          </View>
-      <ScrollView style={{height:"75%"}}>
-     
-        {testItem?.slice(0).reverse().map((item, index) => (
-          <View style={styles.container} key={item.id}>
-            <View >
-              <Image
-                source={{
-                  uri: item.img,
-                }}
-                style={{ width: 100, height: 100 }}
-                resizeMode="contain"
-              />
-            </View>
-            <View style={ {width:"70%"}}>
-              <Text style={styles.text1}>Product: {item.name}</Text>
-              <Text>Price: {item.price} TK</Text>
-              <Text>Quantity: {item.count}</Text>
-            </View>
-            <View>
-              <IconButton
-                style={{
-                  padding: 5,
-                }}
-                icon="delete"
-                iconColor={MD3Colors.error50}
-                size={20}
-                onPress={(e) => removeItem(item.id, item.price, item.count)}
-              />
-            </View>
-          </View>
-        ))}
+          </TouchableOpacity>
+        </View>
+      </View>
 
-      
-
-        
+      <ScrollView style={{ height: "75%" }}>
+        {testItem
+          ?.slice(0)
+          .reverse()
+          .map((item, index) => (
+            <View style={styles.container} key={item.id}>
+              <View style={{width:"20%"}}>
+                <Image
+                  source={{
+                    uri: item.img,
+                  }}
+                  style={{ width: 100, height: 100 }}
+                  resizeMode="contain"
+                />
+              </View>
+              <View style={{ width: "70%" }}>
+                <Text style={styles.text1}>Product: {item.name}</Text>
+                <Text>Price: {item.price} TK</Text>
+                <Text>Quantity: {item.count}</Text>
+              </View>
+              <View style={{width:"10%"}}>
+                <IconButton
+                  style={{
+                    padding: 5,
+                  }}
+                  icon="delete"
+                  iconColor={MD3Colors.error50}
+                  size={20}
+                  onPress={(e) => removeItem(item.id, item.price, item.count)}
+                />
+              </View>
+            </View>
+          ))}
       </ScrollView>
       <View style={styles.qrContainer}>
-        
-      {totalCost > 0 && (
+        {totalCost > 0 && (
           <View>
-            <Text style={{ textAlign: "right", fontWeight: "bold", marginBottom: 10 }}>
+            <Text
+              style={{
+                textAlign: "right",
+                fontWeight: "bold",
+                marginBottom: 10,
+              }}
+            >
               Total: {totalCost} tk
             </Text>
           </View>
         )}
-         
+      </View>
+      <View style={styles.bottomView}>
+        <View>
+          <Button
+            mode="contained"
+            onPress={() => resetAll()}
+            buttonColor="red"
+            style={{ width: 150 }}
+          >
+            Reset
+          </Button>
         </View>
-        <View style={styles.bottomView}>
-            <View>
+        <View>
+          <Button
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+            buttonColor="green"
+            style={{ width: 150 }}
+          >
+            Checkout
+          </Button>
+        </View>
+      </View>
 
-            <Button
-              mode="contained"
-              onPress={() => resetAll()}
-              buttonColor="red"
-              style={{width:150}}
-            >
-              Reset
-            </Button>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+       
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {/* onPress={() => setModalVisible(!modalVisible)} */}
+
+            <View style={{ width: "100%" }}>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <TextInput
+                  style={{ width: "90%" }}
+                  onChangeText={(text) => searchFilterFunction(text)}
+                  value={search}
+                  underlineColorAndroid="transparent"
+                  placeholder="Search names"
+                />
+                <TouchableOpacity>
+                  <AntDesign
+                    name="closecircle"
+                    size={24}
+                    color="black"
+                    onPress={() => setModalVisible(!modalVisible)}
+                  />
+                </TouchableOpacity>
+              </View>
+
+              <FlatList
+              style={{marginTop:20}}
+                data={filteredDataSource}
+                keyExtractor={(item, index) => index.toString()}
+                ItemSeparatorComponent={ItemSeparatorView}
+                renderItem={({ item }) => <ItemView item={item} />}
+              />
             </View>
-           <View>
-           <Button
-              mode="contained"
-              onPress={() => console.log("Pressed")}
-              buttonColor="green"
-             
-              style={{width:150}}
-            >
-              Checkout
-            </Button>
-
-           </View>
-          
           </View>
+        </View>
+      </Modal>
     </NotchDetector>
   );
 }
@@ -237,13 +349,13 @@ const styles = StyleSheet.create({
     borderColor: "#0320fc",
     borderRadius: 5,
     padding: 5,
+    margin: 10,
   },
   qrContainer: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
-    
   },
   width: {
     width: "48%",
@@ -252,11 +364,55 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   bottomView: {
-    display:"flex",
+    display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginLeft:'10%',
-    marginRight:'10%',
+    marginLeft: "10%",
+    marginRight: "10%",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    backgroundColor: "rgba(18, 10, 97, 0.35)",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "100%",
+    height: "90%",
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
